6876da9ca80256e42b2544e304e6411cae9b3eca
lukeadamis <52349519+lukeadamis@users.noreply.github.com>
2019-07-12 15:46:05 +0200

    Update alert.js

--- js/alert.js
+++ js/alert.js
@@ -1,4 +1,1 @@
-alert("Hello!! I come from GitHub via jsDelivr in peace and ♡ ---");
-$(document).ready(function(){
-$("h2").css("background-color", "yellow");
-});
+alert("Hello! I come from GitHub via jsDelivr in peace and ♡");
